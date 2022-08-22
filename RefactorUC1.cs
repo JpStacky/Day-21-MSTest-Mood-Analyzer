650da49432e88c4b282b287153581cfa1e816a62
Jp <jyotiprakashpc@outlook.com>
2022-08-22 10:44:24 +0530

    Analyze sad or happy

---
+++ RefactorUC1.cs
@@ -0,0 +1,20 @@
+﻿namespace MoodAnalyzerProblem
+{
+    public class RefactorUC1
+    {
+        string
+        RefactorUC1()
+        {
+
+        }
+        public string analyseMood(string message)
+        {
+            string result = "Happy Mood";
+            if (message.Contains("sad", StringComparison.OrdinalIgnoreCase))
+                result = "Sad Mood";
+            else
+                result = "Happy Mood";
+            return result;
+        }
+    }
+}
